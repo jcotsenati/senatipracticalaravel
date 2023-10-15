b03434d32dbbc8e506eb54c6f6f17c0452740bc9
Jorge Cotrado <jumanor@gmail.com>
2023-10-14 21:46:17 -0500

    add table cursos,matriculas y usuarios

---
+++ database/migrations/2023_10_15_024030_add_rows_usuarios_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::table('usuarios')->insert([
+            'correo' => 'jorge@gmail.com',
+            'password' => 'jorge',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::table('ususarios')
+        ->where('correo', 'jorge@gmail.com')
+        ->where('password', 'jorge')
+        ->delete();
+    }
+};
